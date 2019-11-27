2c961f05147a58bcf7711d0d5be2fa3c0515dbcf
Jonathan Doyle <jdoyle@guidwire.com>
2019-11-27 17:10:30 +0000

    add image to banner

--- public/chrome_extension/contentScript.js
+++ public/chrome_extension/contentScript.js
@@ -10,7 +10,10 @@ var ribbonDiv = document.createElement("div")
 
 ribbonDiv.classList.add("corner-ribbon", "bottom-right", "sticky", "blue", "shadow")
 
-ribbonDiv.innerHTML = "Show Stats"
+var img = document.createElement("img");
+img.src = chrome.runtime.getURL("chrome_extension/images/stats-dots.svg");
+ribbonDiv.appendChild(img)
+// ribbonDiv.innerHTML = "Show Stats"
 
 ribbonDiv.addEventListener('click', function (event) {
     chrome.runtime.sendMessage({type: "showStats", data: document.all[0].outerHTML});
