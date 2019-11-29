533ac17403ecd50ebc9555fc00064c32ae35d092
Jonathan Doyle <jdoyle@guidwire.com>
2019-11-29 13:54:14 +0000

    remove axios

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@
 
 import React, { Component } from 'react';
 import './App.css';
-import axios from 'axios'
 import TabContainer from './components/tabs/TabContainer'
 import * as chromeExtensionUtil from './util/ChromeExtensionUtil'
 import {ALL} from './constants/constants'
